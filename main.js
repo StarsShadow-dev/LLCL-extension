569c3655c9f15de106a9676a387f2005a978776f
StarsShadow-dev <101425256+StarsShadow-dev@users.noreply.github.com>
2023-11-27 20:59:41 -0500

    added a completionItemProvider!

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ vscode.languages.registerHoverProvider('parallel', {
 		
 		return new Promise((resolve, reject) => {
 			try {
-				const args = ["query", modulePath.join("/"), document.uri.path, text.length, line, character];
+				const args = ["query", "hover", modulePath.join("/"), document.uri.path, text.length, line, character];
 				console.log(compilerPath, args);
 				const childProcess = child_process.spawn(compilerPath, args);
 				
@@ -50,8 +50,87 @@ vscode.languages.registerHoverProvider('parallel', {
 					});
 				});
 				
-				childProcess.stderr.on('data', (data) => {
-					console.error(`stderr: ${data}`);
+				childProcess.on('close', (code) => {
+					console.log(`process exited with code ${code}`);
+					if (code != 0) {
+						resolve({
+							contents: ["childProcess error"]
+						});
+					} else {
+						resolve(undefined);
+					}
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		})
+	}
+});
+
+function getNewCompletion(completionItemKind, label, documentationString) {
+	const newCompletion = new vscode.CompletionItem(label, completionItemKind);
+	newCompletion.documentation = new vscode.MarkdownString(documentationString);
+	return newCompletion;
+}
+
+vscode.languages.registerCompletionItemProvider('parallel', {
+	async provideCompletionItems(document, position, token, context) {
+		console.log("position", position);
+		
+		const text = document.getText();
+		
+		const line = position.c + 1;
+		const character = position.e;
+		console.log("character", character, "line", line);
+		
+		// get the compilerPath
+		const compilerPathConfiguration = vscode.workspace.getConfiguration().get('conf.parallel.compilerPath')
+		let compilerPath = "";
+		if (compilerPathConfiguration.length > 0) {
+			compilerPath = `${vscode.workspace.workspaceFolders[0].uri.path}${compilerPathConfiguration}`;
+		} else {
+			compilerPath = `${homedir}/.Parallel_Lang/Parallel-lang`;
+		}
+		
+		// get the modulePath
+		const modulePath = document.uri.path.split("/");
+		modulePath.pop();
+		console.log(text.length, "\n", modulePath.join("/"), document.uri.path);
+		
+		return new Promise((resolve, reject) => {
+			
+			try {
+				const args = ["query", "suggestions", modulePath.join("/"), document.uri.path, text.length, line, character];
+				console.log(compilerPath, args);
+				const childProcess = child_process.spawn(compilerPath, args);
+				
+				childProcess.stdin.write(text);
+				
+				childProcess.on('error', (error) => {
+					console.log(`error: ${error}`);
+				});
+				
+				childProcess.stdout.on('data', (data) => {
+					try {
+						let completionItems = [];
+						
+						console.log("data", `${data}`);
+						
+						const array = JSON.parse(`${data}`);
+						
+						for (let i = 0; i < array.length; i++) {
+							const e = array[i];
+							completionItems.push(getNewCompletion(e[0], e[1], e[2]))
+						}
+						
+						console.log("completionItems", completionItems);
+						resolve(completionItems);
+					} catch (error) {
+						resolve({
+							contents: ["childProcess stdout error"]
+						});
+						console.log(error);
+					}
 				});
 				
 				childProcess.on('close', (code) => {
@@ -60,6 +139,8 @@ vscode.languages.registerHoverProvider('parallel', {
 						resolve({
 							contents: ["childProcess error"]
 						});
+					} else {
+						resolve(undefined);
 					}
 				});
 			} catch (error) {
