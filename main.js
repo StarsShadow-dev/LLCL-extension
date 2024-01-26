56f1bca0bfb3b3acf9d1735fa9fcf8f770d2877e
StarsShadow-dev <101425256+StarsShadow-dev@users.noreply.github.com>
2024-01-26 13:43:33 -0500

    kind of got hover working

--- main.js
+++ main.js
@@ -9,62 +9,108 @@ console.log("setting up the LLCL VS Code Extension");
 
 let diagnosticCollection = vscode.languages.createDiagnosticCollection('go');
 
-console.log("diagnosticCollection", diagnosticCollection);
+function getCompilerPath() {
+	const compilerPathConfiguration = vscode.workspace.getConfiguration().get('conf.llcl_vscode.compilerPath')
+	let compilerPath = "";
+	if (compilerPathConfiguration.length > 0) {
+		if (compilerPathConfiguration[0] == "~") {
+			let newCompilerPath = compilerPathConfiguration.split("");
+			newCompilerPath.shift();
+			compilerPath = `${homedir}${newCompilerPath.join("")}`;
+		} else {
+			compilerPath = `${vscode.workspace.workspaceFolders[0].uri.path}${compilerPathConfiguration}`;
+		}
+	} else {
+		compilerPath = `${homedir}/.LLCL/LLCL`;
+	}
+	return compilerPath;
+}
 
-// vscode.languages.registerHoverProvider('llcl', {
-// 	async provideHover(document, position, token) {
-// 		// get the document text (it may not have been saved to the file system yet)
-// 		const text = document.getText();
+vscode.languages.registerHoverProvider('llcl', {
+	async provideHover(document, position, token) {
+		// get the document text (it may not have been saved to the file system yet)
+		const text = document.getText();
 		
-// 		// get the hover location
-// 		const line = position.e;
-// 		const character = position.c;
-// 		console.log("character", character, "line", line);
+		// get the hover location
+		const line = position.line + 1;
+		const character = position.c;
+		console.log("line", line, "character", character);
 		
-// 		// get the compilerPath
-// 		const compilerPathConfiguration = vscode.workspace.getConfiguration().get('conf.llcl_vscode.compilerPath')
-// 		let compilerPath = "";
-// 		if (compilerPathConfiguration.length > 0) {
-// 			compilerPath = `${vscode.workspace.workspaceFolders[0].uri.path}${compilerPathConfiguration}`;
-// 		} else {
-// 			compilerPath = `${homedir}/.LLCL/LLCL`;
-// 		}
+		// get the compilerPath
+		const compilerPath = getCompilerPath();
 		
-// 		return new Promise((resolve, reject) => {
-// 			try {
-// 				const args = ["query", "hover", document.uri.path, text.length, line, character];
-// 				console.log(compilerPath, args);
-// 				const childProcess = child_process.spawn(compilerPath, args);
+		return new Promise((resolve, reject) => {
+			let gotData = false;
+			
+			try {
+				const args = ["query", "hover", document.uri.path, text.length, line, character];
+				console.log(compilerPath, args);
+				const childProcess = child_process.spawn(compilerPath, args);
 				
-// 				childProcess.stdin.write(text);
+				childProcess.stdin.write(text);
 				
-// 				childProcess.on('error', (error) => {
-// 					console.log(`error: ${error}`);
-// 				});
+				childProcess.on('error', (error) => {
+					console.log(`error: ${error}`);
+				});
 				
-// 				childProcess.stdout.on('data', (data) => {
-// 					console.log(`stdout: ${data}`);
-// 					resolve({
-// 						contents: [`${data}`]
-// 					});
-// 				});
+				childProcess.stdout.on('data', (data) => {
+					console.log("hover data", `${data}`, JSON.parse(`${data}`));
+					
+					gotData = true;
+					try {
+						const array = JSON.parse(`${data}`);
+						
+						let contents = [];
+						
+						let diagnosticList = [];
+						
+						for (let i = 0; i < array.length; i++) {
+							const e = array[i];
+							if (e[0] == 0) {
+								diagnosticList.push(
+									new vscode.Diagnostic(new vscode.Range(
+										new vscode.Position(e[1] - 1, e[2]),
+										new vscode.Position(e[1] - 1, e[3])),
+										e[4]
+									)
+								);
+							} else if (e[0] == 2) {
+								contents.push(new vscode.MarkdownString(e[3]));
+							}
+						}
+						
+						diagnosticCollection.clear();
+						diagnosticCollection.set(document.uri, diagnosticList);
+						
+						resolve({
+							contents: contents
+						});
+					} catch (error) {
+						console.log("error", error);
+						resolve({
+							contents: ["childProcess stdout error"]
+						});
+					}
+				});
 				
-// 				childProcess.on('close', (code) => {
-// 					console.log(`process exited with code ${code}`);
-// 					if (code != 0) {
-// 						resolve({
-// 							contents: ["childProcess error"]
-// 						});
-// 					} else {
-// 						resolve(undefined);
-// 					}
-// 				});
-// 			} catch (error) {
-// 				console.log(error);
-// 			}
-// 		})
-// 	}
-// });
+				childProcess.on('close', (code) => {
+					console.log(`process exited with code ${code}`);
+					if (!gotData) {
+						if (code != 0) {
+							resolve({
+								contents: ["childProcess error"]
+							});
+						} else {
+							resolve(undefined);
+						}
+					}
+				});
+			} catch (error) {
+				console.log(error);
+			}
+		})
+	}
+});
 
 function getNewCompletion(completionItemKind, label, documentationString) {
 	vscode.CompletionItemKind;
@@ -76,32 +122,15 @@ function getNewCompletion(completionItemKind, label, documentationString) {
 
 vscode.languages.registerCompletionItemProvider('llcl', {
 	async provideCompletionItems(document, position, token, context) {
-		console.log("position", position);
-		// console.log("token", token);
-		// console.log("context", context);
-		
-		// console.time("registerCompletionItemProvider");
-		
+		// get the document text (it may not have been saved to the file system yet)
 		const text = document.getText();
 		
-		const line = position.c + 1;
+		const line = position.line + 1;
 		const character = position.e;
-		console.log("character", character, "line", line);
+		console.log("line", line, "character", character);
 		
 		// get the compilerPath
-		const compilerPathConfiguration = vscode.workspace.getConfiguration().get('conf.llcl_vscode.compilerPath')
-		let compilerPath = "";
-		if (compilerPathConfiguration.length > 0) {
-			if (compilerPathConfiguration[0] == "~") {
-				let newCompilerPath = compilerPathConfiguration.split("");
-				newCompilerPath.shift();
-				compilerPath = `${homedir}${newCompilerPath.join("")}`;
-			} else {
-				compilerPath = `${vscode.workspace.workspaceFolders[0].uri.path}${compilerPathConfiguration}`;
-			}
-		} else {
-			compilerPath = `${homedir}/.LLCL/LLCL`;
-		}
+		const compilerPath = getCompilerPath();
 		
 		return new Promise((resolve, reject) => {
 			let gotData = false;
@@ -117,38 +146,39 @@ vscode.languages.registerCompletionItemProvider('llcl', {
 				});
 				
 				childProcess.stdout.on('data', (data) => {
+					console.log("suggestions data", `${data}`);
+					
 					gotData = true;
 					try {
 						let completionItems = [];
 						
-						console.log("data", `${data}`);
-						
 						const array = JSON.parse(`${data}`);
 						
-						diagnosticCollection.clear();
+						let diagnosticList = [];
 						
 						for (let i = 0; i < array.length; i++) {
 							const e = array[i];
 							if (e[0] == 0) {
-								diagnosticCollection.set(document.uri, [
+								diagnosticList.push(
 									new vscode.Diagnostic(new vscode.Range(
 										new vscode.Position(e[1] - 1, e[2]),
 										new vscode.Position(e[1] - 1, e[3])),
 										e[4]
 									)
-								]);
+								);
 							} else if (e[0] == 2) {
 								completionItems.push(getNewCompletion(e[1], e[2], e[3]))	
 							}
 						}
 						
+						diagnosticCollection.clear();
+						diagnosticCollection.set(document.uri, diagnosticList);
+						
 						console.log("completionItems", completionItems);
 						resolve(completionItems);
 					} catch (error) {
-						resolve({
-							contents: ["childProcess stdout error"]
-						});
 						console.log(error);
+						resolve(undefined);
 					}
 				});
 				
@@ -157,13 +187,7 @@ vscode.languages.registerCompletionItemProvider('llcl', {
 					
 					console.log(`process exited with code ${code}`);
 					if (!gotData) {
-						if (code != 0) {
-							resolve({
-								contents: ["childProcess error"]
-							});
-						} else {
-							resolve(undefined);
-						}	
+						resolve(undefined);
 					}
 				});
 			} catch (error) {
